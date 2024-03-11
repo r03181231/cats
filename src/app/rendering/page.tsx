77b98e12353dae144ad6122d280085229987ff09
HaeRheeNam <4ran0318@gmail.com>
2024-03-11 21:38:26 +0900

    [Feat] 라우팅 설정

---
+++ src/app/rendering/page.tsx
@@ -0,0 +1,13 @@
+// src>app>rendering/page.tsx
+
+import React from "react";
+
+const RenderingTestPage = () => {
+  return (
+    <div>
+      <h1>4가지 렌더링 방식을 테스트합니다.</h1>
+    </div>
+  );
+};
+
+export default RenderingTestPage;
